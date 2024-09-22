b86923f2c2b962a219ba925a541a53690b375eba
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-09-23 03:14:36 +0400

    switched react-router-sitemap to sitemap-generator

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -1,16 +1,9 @@
-require('babel-register')({
-    presets: ['es2015', 'react']
-  });
-  
-  const router = require('./src/App').default;
-  const Sitemap = require('react-router-sitemap').default;
-  
-  function generateSitemap() {
-    return (
-      new Sitemap(router)
-          .build('https://artedelpulito.vercel.app/')
-          .save('./public/sitemap.xml')
-    );
-  }
-  
-  generateSitemap();
+import SitemapGenerator from 'sitemap-generator';
+
+const generator = SitemapGenerator('https://artedelpulito.vercel.app', {
+  filepath: './public/sitemap.xml',
+  maxEntriesPerFile: 50000,
+  stripQuerystring: true,
+});
+
+generator.start();
