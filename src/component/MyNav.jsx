b6ac654e85c5a1ef1edcfb48898e6250b67842e1
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-09-22 16:29:51 +0200

    nav bar

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -1,4 +1,27 @@
+import { Container, Navbar } from "react-bootstrap";
+import logo from "../assets/laundry.svg";
+import { FaWhatsapp } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import style from "../component/MyNav.module.css";
 const MyNav = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar expand="sm">
+        <Container>
+          <Navbar.Brand href="#home" className="d-flex align-items-center">
+            <img src={logo} width="30" height="30" className="d-inline-block align-top me-3" alt=" logo" />
+            <h5>Arte del pulito</h5>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <FaWhatsapp size={24} className={`${style.iconColor} me-3`} />
+              <CiFacebook size={24} className={style.iconColor} />
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 export default MyNav;
