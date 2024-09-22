fd9ff9d250134a88189932e9c976933bf157aaa7
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-09-22 22:39:53 +0400

    updated navbar whatsapp icon

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -1,20 +1,30 @@
 import { Container, Navbar } from "react-bootstrap";
-import logo from "../assets/laundry.svg";
 import { FaWhatsapp } from "react-icons/fa";
 import { CiFacebook } from "react-icons/ci";
 import styles from "./MyNav.module.css";
+
 const MyNav = () => {
+  const handleWhatsAppClick = () => {
+    const phoneNumber = '3911220571';
+    const message = 'Salve, vorrei informazioni sui vostri servizi di lavanderia.'; 
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(whatsappUrl, '_blank');
+  };
+
   return (
-    <>
-      <Navbar expand="sm" className={styles.navBar}>
-        <Container>
-          <Navbar.Brand href="#home" className={` d-flex align-items-center`}>
-            <FaWhatsapp size={34} className={`${styles.iconColor} me-3`} />
-            <CiFacebook size={34} className={styles.iconColor} />
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar expand="sm" className={styles.navBar}>
+      <Container>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <FaWhatsapp 
+            size={34} 
+            className={`${styles.iconColor} ${styles.whatsappIcon} me-3`} 
+            onClick={handleWhatsAppClick}
+          />
+          <CiFacebook size={34} className={styles.iconColor} />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 };
+
 export default MyNav;
