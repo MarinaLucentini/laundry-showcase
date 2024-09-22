bf5a32b83a2f1922fafb2a22818362c24acd688c
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-09-22 20:15:13 +0200

    bubble animation

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -2,23 +2,16 @@ import { Container, Navbar } from "react-bootstrap";
 import logo from "../assets/laundry.svg";
 import { FaWhatsapp } from "react-icons/fa";
 import { CiFacebook } from "react-icons/ci";
-import style from "../component/MyNav.module.css";
+import styles from "./MyNav.module.css";
 const MyNav = () => {
   return (
     <>
-      <Navbar expand="sm">
+      <Navbar expand="sm" className={styles.navBar}>
         <Container>
-          <Navbar.Brand href="#home" className="d-flex align-items-center">
-            <img src={logo} width="30" height="30" className="d-inline-block align-top me-3" alt=" logo" />
-            <h5>Arte del pulito</h5>
+          <Navbar.Brand href="#home" className={` d-flex align-items-center`}>
+            <FaWhatsapp size={34} className={`${styles.iconColor} me-3`} />
+            <CiFacebook size={34} className={styles.iconColor} />
           </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              <FaWhatsapp size={24} className={`${style.iconColor} me-3`} />
-              <CiFacebook size={24} className={style.iconColor} />
-            </Navbar.Text>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './MainPage.module.css';
-import laundryIllustration from '../assets/laundry_1.svg';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./MainPage.module.css";
+import laundryIllustration from "../assets/laundry_1.svg";
 
 const ServiceTile = ({ title, description }) => (
   <div className={styles.serviceTile}>
@@ -19,34 +19,48 @@ const MainPage = () => {
   const services = [
     {
       title: "Tintoria Lavanderia",
-      description: "Ravviva i colori e dì addio alle macchie! Tecniche all'avanguardia per capi come nuovi."
+      description: "Ravviva i colori e dì addio alle macchie! Tecniche all'avanguardia per capi come nuovi.",
     },
     {
       title: "Lavanderia ad Acqua",
-      description: "Pulizia delicata e profonda con detergenti eco-friendly. Freschezza garantita per ogni capo."
+      description: "Pulizia delicata e profonda con detergenti eco-friendly. Freschezza garantita per ogni capo.",
     },
     {
       title: "Lavanderia a Secco",
-      description: "Trattamento esperto per capi preziosi. Preserviamo tessuti delicati e abiti eleganti."
+      description: "Trattamento esperto per capi preziosi. Preserviamo tessuti delicati e abiti eleganti.",
     },
     {
       title: "Stireria Professionale",
-      description: "Finitura impeccabile per ogni tessuto. Eleganza senza sforzo, per ogni occasione."
-    }
+      description: "Finitura impeccabile per ogni tessuto. Eleganza senza sforzo, per ogni occasione.",
+    },
   ];
 
   return (
     <div className={styles.mainPage}>
       <header className={styles.headerSection}>
+        <div className={styles.bubbleBackground}>
+          <div id="background-wrap">
+            <div className={`${styles.bubble} ${styles.x1}`}></div>
+            <div className={`${styles.bubble} ${styles.x2}`}></div>
+            <div className={`${styles.bubble} ${styles.x3}`}></div>
+            <div className={`${styles.bubble} ${styles.x4}`}></div>
+            <div className={`${styles.bubble} ${styles.x5}`}></div>
+            <div className={`${styles.bubble} ${styles.x6}`}></div>
+            <div className={`${styles.bubble} ${styles.x7}`}></div>
+            <div className={`${styles.bubble} ${styles.x8}`}></div>
+            <div className={`${styles.bubble} ${styles.x9}`}></div>
+            <div className={`${styles.bubble} ${styles.x10}`}></div>
+          </div>
+        </div>
         <div className={styles.headerContent}>
           <img src={laundryIllustration} alt="Laundry Illustration" className={styles.laundryIllustration} />
           <div className={styles.titleContainer}>
             <h1 className={styles.companyName}>Arte del Pulito</h1>
-            <p className={styles.tagline}>Non Self Service</p>
+            <p className={styles.tagline}>Perché stressarsi con il bucato? Noi lo facciamo meglio!</p>
           </div>
         </div>
       </header>
-      
+
       <main className={styles.mainContent}>
         <div className={styles.servicesGrid}>
           {services.map((service, index) => (
@@ -56,7 +70,9 @@ const MainPage = () => {
       </main>
 
       <footer className={styles.footer}>
-      <p className={styles.address}>Via delle Murene 9, Tor San Lorenzo <br/> +39 0691010970 <br/> giomon8367@gmail.com</p>
+        <p className={styles.address}>
+          Via delle Murene 9, Tor San Lorenzo <br /> +39 0691010970 <br /> giomon8367@gmail.com
+        </p>
         <div className={styles.mapSection}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.805359290274!2d12.54562931541677!3d41.60170679246456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13258d5df5b315d3%3A0x7f4b5582df6a8188!2sVia%20delle%20Murene%2C%209%2C%2000040%20Torvaianica%20RM%2C%20Italy!5e0!3m2!1sen!2sus!4v1632501144251!5m2!1sen!2sus"
