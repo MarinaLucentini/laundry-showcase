165022df73735cd73450187a84c6ceb5d247e0e1
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-09-22 21:30:01 +0200

    update

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -5,21 +5,18 @@ import styles from "./MyNav.module.css";
 
 const MyNav = () => {
   const handleWhatsAppClick = () => {
-    const phoneNumber = '3911220571';
-    const message = 'Salve, vorrei informazioni sui vostri servizi di lavanderia.'; 
+    const phoneNumber = "3911220571";
+    const message = "Salve, vorrei informazioni sui vostri servizi di lavanderia.";
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.open(whatsappUrl, '_blank');
+
+    window.open(whatsappUrl, "_blank");
   };
 
   return (
     <Navbar expand="sm" className={styles.navBar}>
       <Container>
         <Navbar.Brand href="#home" className="d-flex align-items-center">
-          <FaWhatsapp 
-            size={34} 
-            className={`${styles.iconColor} ${styles.whatsappIcon} me-3`} 
-            onClick={handleWhatsAppClick}
-          />
+          <FaWhatsapp size={34} className={`${styles.iconColor} ${styles.whatsappIcon} me-3`} onClick={handleWhatsAppClick} />
           <CiFacebook size={34} className={styles.iconColor} />
         </Navbar.Brand>
       </Container>
