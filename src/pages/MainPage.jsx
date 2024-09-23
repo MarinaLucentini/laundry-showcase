6a9f1319976d7a8df2257cdcb9dd75c304633478
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-09-23 19:06:58 +0400

    upated iframe embed map

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -76,19 +76,20 @@ const MainPage = () => {
           Via delle Murene 9, Tor San Lorenzo <br /> +39 0691010970 <br /> giomon8367@gmail.com
         </p>
         <div className={styles.mapSection}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.805359290274!2d12.54562931541677!3d41.60170679246456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13258d5df5b315d3%3A0x7f4b5582df6a8188!2sVia%20delle%20Murene%2C%209%2C%2000040%20Torvaianica%20RM%2C%20Italy!5e0!3m2!1sen!2sus!4v1632501144251!5m2!1sen!2sus"
-            width="100%"
-            height="460"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            title="Company Location"
-          ></iframe>
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d186.57428350252124!2d12.538311!3d41.565164!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1325960c4d17776d%3A0x2abdb9e537dcf9c0!2sArte%20del%20pulito!5e0!3m2!1sen!2sus!4v1727103864673!5m2!1sen!2sus" 
+          width="100%" 
+          height="460" 
+          style={{ border: 0 }}
+          allowfullscreen="" 
+          loading="lazy" 
+          title="Company Location"
+        ></iframe>
         </div>
       </footer>
     </div>
   );
 };
+
 
 export default MainPage;
