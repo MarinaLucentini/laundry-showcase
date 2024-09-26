9db5c0e5de92743f1d2a50b386848a2c6d71d0ca
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-09-26 22:22:07 +0200

    update

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import { CustomerList, CustomerEdit, CustomerCreate } from './Customers';
-import { AdminTheme } from './AdminTheme';
-import jsonServerProvider from 'ra-data-json-server';
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import { CustomerList, CustomerEdit, CustomerCreate } from "./Customers";
+import { AdminTheme } from "./AdminTheme";
+import jsonServerProvider from "ra-data-json-server";
 
 // Replace this URL with the actual API endpoint
-const dataProvider = jsonServerProvider('http://localhost:3000');
+const dataProvider = jsonServerProvider("http://localhost:3001");
 
 const AdminPanel = () => (
   <Admin basename="/admin" dataProvider={dataProvider} theme={AdminTheme}>
-    <Resource 
-      name="customers" 
-      list={CustomerList} 
-      edit={CustomerEdit} 
-      create={CustomerCreate} 
-    />
+    <Resource name="customers" list={CustomerList} edit={CustomerEdit} create={CustomerCreate} />
   </Admin>
 );
 
