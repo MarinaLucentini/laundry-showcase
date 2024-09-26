ebe15d20ae5233c643dc0da9a3b56c13a6e5535f
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-09-27 00:40:46 +0200

    update

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./MainPage.module.css";
 import laundryIllustration from "../assets/laundry_1.svg";
-import StructuredData from '../component/StructuredData';
+import StructuredData from "../component/StructuredData";
 
 const ServiceTile = ({ title, description }) => (
   <div className={styles.serviceTile}>
@@ -28,7 +28,7 @@ const MainPage = () => {
     },
     {
       title: "Lavanderia a Secco",
-      description: "Trattamento esperto per capi preziosi. Preserviamo tessuti delicati e abiti eleganti.",
+      description: "Trattamento esperto per capi delicati. Preserviamo tessuti delicati e abiti eleganti.",
     },
     {
       title: "Stireria Professionale",
@@ -76,20 +76,19 @@ const MainPage = () => {
           Via delle Murene 9, Tor San Lorenzo <br /> +39 0691010970 <br /> giomon8367@gmail.com
         </p>
         <div className={styles.mapSection}>
-        <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d186.57428350252124!2d12.538311!3d41.565164!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1325960c4d17776d%3A0x2abdb9e537dcf9c0!2sArte%20del%20pulito!5e0!3m2!1sen!2sus!4v1727103864673!5m2!1sen!2sus" 
-          width="100%" 
-          height="460" 
-          style={{ border: 0 }}
-          allowFullScreen="" 
-          loading="lazy" 
-          title="Company Location"
-        ></iframe>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d186.57428350252124!2d12.538311!3d41.565164!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1325960c4d17776d%3A0x2abdb9e537dcf9c0!2sArte%20del%20pulito!5e0!3m2!1sen!2sus!4v1727103864673!5m2!1sen!2sus"
+            width="100%"
+            height="460"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            title="Company Location"
+          ></iframe>
         </div>
       </footer>
     </div>
   );
 };
-
 
 export default MainPage;
