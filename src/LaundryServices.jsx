3712dd8a05fbbf529eb16b61e2fa5ef6c7fe517d
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-09-27 21:48:50 +0400

    updates

--- src/LaundryServices.jsx
+++ src/LaundryServices.jsx
@@ -1,28 +1,74 @@
 import React from "react";
-import { List, Datagrid, TextField, TextInput, Edit, Create, SimpleForm } from "react-admin";
+import {
+  List,
+  Datagrid,
+  TextField,
+  TextInput,
+  Edit,
+  Create,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  useRefresh,
+  useNotify,
+  required,
+  minLength,
+} from "react-admin";
 
 export const LaundryServiceList = (props) => (
   <List {...props}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <TextField source="name" label="Service Name" />
+      <TextField source="customer.name" label="Associated Customer" />
     </Datagrid>
   </List>
 );
 
-export const LaundryServiceCreate = (props) => (
-  <Create {...props}>
-    <SimpleForm>
-      <TextInput source="name" label="Service Name" />
-    </SimpleForm>
-  </Create>
-);
+export const LaundryServiceCreate = (props) => {
+  const notify = useNotify();
+  const refresh = useRefresh();
 
-export const LaundryServiceEdit = (props) => (
-  <Edit {...props}>
-    <SimpleForm>
-      <TextInput disabled source="id" />
-      <TextInput source="name" label="Service Name" />
-    </SimpleForm>
-  </Edit>
-);
+  const onSuccess = () => {
+    notify('Service created successfully!');
+    refresh();
+  };
+
+  const onFailure = (error) => {
+    notify(`Error: ${error.message}`, 'warning');
+  };
+
+  return (
+    <Create {...props} onSuccess={onSuccess} onFailure={onFailure}>
+      <SimpleForm>
+        <TextInput source="name" label="Service Name" validate={[required(), minLength(3)]} />
+      </SimpleForm>
+    </Create>
+  );
+};
+
+export const LaundryServiceEdit = (props) => {
+  const notify = useNotify();
+  const refresh = useRefresh();
+
+  const onSuccess = () => {
+    notify('Service updated successfully!');
+    refresh();
+  };
+
+  const onFailure = (error) => {
+    notify(`Error: ${error.message}`, 'warning');
+  };
+
+  return (
+    <Edit {...props} onSuccess={onSuccess} onFailure={onFailure}>
+      <SimpleForm>
+        <TextInput disabled source="id" />
+        <TextInput source="name" label="Service Name" validate={[required(), minLength(3)]} />
+        <ReferenceInput label="Associated Customer" source="customerId" reference="customers">
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+      </SimpleForm>
+    </Edit>
+  );
+};
